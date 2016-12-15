71cd241f88d6648778d9069d8164180b21a0d7eb
Kenzo Nirasawa <kemuzo.nira@gmail.com>
2016-12-15 21:13:48 +0900

    first commit

--- TinderUISample/AppDelegate.swift
+++ TinderUISample/AppDelegate.swift
@@ -15,7 +15,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        self.window = UIWindow(frame: UIScreen.main.bounds)
+        let mainVC = ViewController()
+        let navVC = UINavigationController(rootViewController: mainVC)
+        self.window?.rootViewController = navVC
+        self.window!.makeKeyAndVisible()
         return true
     }
 
